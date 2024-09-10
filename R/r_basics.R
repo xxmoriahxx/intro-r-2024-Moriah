c6ad660d1aec46684013a6f5f0466b0a72327395
xxmoriahxx <xxmoriahxx@gmail.com>
2024-09-10 15:18:22 -0700

    worked on operators and comparisons

--- R/r_basics.R
+++ R/r_basics.R
@@ -7,6 +7,37 @@
 42
 
 # Operators ----
+2 + 3 # note the spacing
+2 - 3
+2 * 3 # multiplication
+2 / 3
+2 ** 3 # be careful, this is an exponent
+2 ^ 3
+
+#Comparison
+2 == 2
+"joe" == "Joe"  # false because R is case sensitive
+2 == 1 + 1  # Okay
+2 == (1 + 1)   # Better because it's clearer
+2 != 1 # TRUE statement because exclamation is negation
+2 >= 3 # FALSE statement
+TRUE == 1 # yes because 1 means TRUE
+FALSE == 0 # yes
+isTRUE(TRUE) # yes it is
+isTRUE(1) # no it's not
+?isTRUE # queries built-in help
+
+2 < 3 & 2 > 3 # both have to pass the test, so it's false
+2 < 3 | 2 > 3 # either on can be true
+2 < 3 & (2 == 1 | 2 == 2) # grouping statements for ordering
+
+# type matters (sometimes)
+"joe" # string or character type
+typeof("joe") # numeric type
+typeof(42) # numeric type (double precision, floating to the decimal point)
+typeof(TRUE) # logical or Boolean
+42 == "42" # it's TRUE - equality across types
+identical(42, "42") # no, type matters for identicalness
 
 # variables ----
 
