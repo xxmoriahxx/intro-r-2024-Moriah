58d381f801ff45489c0b1988750a41a699c5e061
xxmoriahxx <xxmoriahxx@gmail.com>
2024-09-11 11:08:30 -0700

    dataframes, pipes, filters

--- R/intro-libraries-imports.R
+++ R/intro-libraries-imports.R
@@ -14,3 +14,30 @@ summary(sta_meta) # see if min max out of range, if you have unexpected NAs
 > library(readxl)
 > icebreaker_answers <- read_excel("data/icebreaker_answers.xlsx")
 > View(icebreaker_answers)
+
+library(dplyr)
+
+odot_meta <- sta_meta |> # old pipe notation was %>%
+  filter(
+    agency == "ODOT",
+         highwayid == 1
+    )
+
+notodot_meta <- sta_meta |>
+  filter(
+    agency != "ODOT"
+  )
+
+# looking for NAs
+nas_meta <- sta_meta |>
+  filter (
+    is.na(detectorlocation)
+  )
+
+# excluding NAs
+real_meta <- sta_meta |>
+  filter(
+    !is.na(detectorlocation
+           )
+  )
+
