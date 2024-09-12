ddf232a7ea721a35cfe9b72f947766c4c2d7334b
xxmoriahxx <xxmoriahxx@gmail.com>
2024-09-12 09:42:30 -0700

    finishing up joins

---
+++ R/intro_joins.R
@@ -0,0 +1,31 @@
+library(dplyr)
+library(ggplot2)
+
+#import data sets
+detectors <- read.csv("data/portal_detectors.csv", stringsAsFactors = F)
+stations <- read.csv("data/portal_stations.csv", stringsAsFactors = F)
+data <- read.csv("data/agg_data.csv", stringsAsFactors = F)
+
+head(data)
+table (data$detector_id)
+
+data_detectors <- data |>
+  distinct(detector_id)
+
+data_detectors_meta <- data_detectors |>
+  left_join(detectors, by = c("detector_id" = "detectorid"))
+
+data_detectors_missing <- detectors |>
+  anti_join(data_detectors, by = c("detectorid" = "detector_id")) |>
+  distinct(detectorid)
+
+# use the data_detectors_meta to join with the stations metadata - find common attribute
+# stationid is common variable
+# I want the stations metadata for the detectors that we have data for
+# add the info from the stations metadata to the detector metadata so that it has all the info about the station
+
+data_stations <- data_detectors_meta |>
+  select (detector_id, stationid) |>
+  left_join (stations, by = "stationid") #don't need c because variable has same name both files
+head(data_stations)
+# to the table about the detectors, add the information about the station (a station has multiple detectors)
